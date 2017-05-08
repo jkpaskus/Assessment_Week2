b4e6e620cd0aa417d1fd88a8127886eb0d26150f
Jonas <jkpaskus@gmail.com>
2017-05-07 21:04:21 -0400

    Initial submission

---
+++ Assessment_Week2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter how many stars you want displayed: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\n\n You have chosen to display {0} stars", n);
+        }
+    }
+}
